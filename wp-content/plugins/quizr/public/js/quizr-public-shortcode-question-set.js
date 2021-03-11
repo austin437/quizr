811beccf61b4817800729e58dd9e4c79befc7662
Robert Austin <austin437@hotmail.com>
2021-03-11 16:29:15 +0000

    quizr-public-shortcode-api.php: updated

--- wp-content/plugins/quizr/public/js/quizr-public-shortcode-question-set.js
+++ wp-content/plugins/quizr/public/js/quizr-public-shortcode-question-set.js
@@ -1,75 +1,102 @@
 class Quizr_Public_Shortcode_Question_Set {
     constructor(element) {
-        this.element = element;        
-        this.init();       
-
+        this.element = element;
+        this.init();
     }
 
     init() {
-        console.log('init');
-        this.articles = this.element.querySelectorAll("article");
+        this.start_quiz_link = this.element.querySelector(".quizr-shortcode-question-set__intro__start-quiz");
+        this.intro = this.element.querySelector(".quizr-shortcode-question-set__intro");
+        this.questions = this.element.querySelector(".quizr-shortcode-question-set__questions");
+        this.articles = this.element.querySelectorAll(".quizr-shortcode-question-set__questions article");
         this.next_arrow = this.element.querySelector(".quizr-shortcode-question-set__arrows__next");
         this.prev_arrow = this.element.querySelector(".quizr-shortcode-question-set__arrows__prev");
         this.pips = this.element.querySelectorAll(".quizr-shortcode-question-set__pips__pip");
         this.index = 0;
         this.minItems = 0;
         this.maxItems = this.pips.length;
-        this.hideAllArticles();
-        if (this.articles.length > 0) this.showArticle(this.index);
+        this.updateHtml();
         this.addEventListeners();
     }
 
-    nextSlide() {}
-
-    previousSlide() {}
-
-
-
-    showArticle( index ) {
-        this.articles[index].classList.add("show");
+    startQuiz(){
+        this.intro.classList.remove('show');
+        this.questions.classList.add("show");
+        
     }
 
-    hideArticle( index ) {
-        this.articles[index].classList.remove("show");
+    updateHtml() {
+        this.hideAllArticles();
+        this.showArticle();
+        this.showArrows();
+        this.updatePips();
+    }
+
+    showArticle() {
+        this.articles[this.index].classList.add("show");
     }
 
     hideAllArticles() {
-        for (let i=0; i<this.articles.length; i++) {
-            this.hideArticle(i);
+        for (let i = 0; i < this.articles.length; i++) {
+            this.articles[i].classList.remove("show");
         }
     }
 
-    showArrows(){
-        this.showNextArrow( parseInt(this.index) !== this.maxItems-1);
-        this.showPreviousArrow( parseInt(this.index) !== 0);
+    updatePips() {
+        for (let i = 0; i < this.pips.length; i++) {
+            this.pips[i].classList.remove("active");
+        }
+        this.pips[this.index].classList.add("active");
     }
 
-    showNextArrow( show ){
-        this.next_arrow.classList.remove('show');
-        if( show ) this.next_arrow.classList.add('show');
+    showArrows() {
+        this.showNextArrow(parseInt(this.index) !== this.maxItems - 1);
+        this.showPreviousArrow(parseInt(this.index) !== 0);
     }
 
-    showPreviousArrow( show ){
+    showNextArrow(show) {
+        this.next_arrow.classList.remove("show");
+        if (show) this.next_arrow.classList.add("show");
+    }
+
+    showPreviousArrow(show) {
         this.prev_arrow.classList.remove("show");
         if (show) this.prev_arrow.classList.add("show");
     }
 
-    handleNextClick(){
-        if( parseInt(this.index) < parseInt(this.maxItems-1) ) this.index++;     
-        this.hideAllArticles();
-        this.showArticle( this.index );
-        this.showArrows();
+    handleNextClick() {
+        if (parseInt(this.index) < parseInt(this.maxItems - 1)) this.index++;
+        this.updateHtml();
     }
 
-    handlePrevClick(){
-        if (parseInt(this.index) > 0 ) this.index--;
-        this.hideAllArticles();
-        this.showArticle(this.index);
-        this.showArrows();
+    handlePrevClick() {
+        if (parseInt(this.index) > 0) this.index--;
+        this.updateHtml();
     }
 
-    addEventListeners(){        
-        this.next_arrow.addEventListener( 'click', (ev) => {ev.preventDefault(); this.handleNextClick() } );
-        this.prev_arrow.addEventListener( 'click', (ev) => {ev.preventDefault(); this.handlePrevClick() } );
+    handlePipClick(i) {
+        this.index = i;
+        this.updateHtml();
+    }
+
+    addEventListeners() {
+        this.start_quiz_link.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            this.startQuiz();
+        });
+        this.next_arrow.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            this.handleNextClick();
+        });
+        this.prev_arrow.addEventListener("click", (ev) => {
+            ev.preventDefault();
+            this.handlePrevClick();
+        });
+        for (let i = 0; i < this.pips.length; i++) {
+            this.pips[i].firstChild.addEventListener("click", (ev) => {
+                ev.preventDefault();
+                this.handlePipClick(i);
+            });
+        }
     }
 }
--- wp-content/plugins/quizr/public/lib/apis/class-quizr-shortcodes-api.php
+++ wp-content/plugins/quizr/public/lib/apis/class-quizr-shortcodes-api.php
@@ -23,70 +23,72 @@ class Quizr_Shortcodes_Api {
        
         ?>
 
-        <div class="quizr-shortcode-question-set">
-           
-            <article class="hide">
-                <aside>
-                    <img src="https://www.quizzer.dev.cc/wp-content/plugins/quizr/public/img/quizr-logo.png" />   
-                </aside>     
-                <main>
-                    <header> 
-                        <h2>Time for a Quizr</h2>
-                    </header> 
-                    <section>
-                        <p>You will be shown some multiple choice questions</p>
-                        <p>Choose your answer and then move on.</p>    
-                        <p>You can move backwards and forwards amongst the questions before submitting your answers at the end</p>
-                    </section>    
-                </main>
-            </article>
-
-            <?php foreach( $questions as $index => $q ){ ?>
-                <article class="hide show">
+        <div class="quizr-shortcode-question-set"> 
+            <div class="quizr-shortcode-question-set__intro hide show">
+                <article class="hide show"> 
                     <aside>
                         <img src="https://www.quizzer.dev.cc/wp-content/plugins/quizr/public/img/quizr-logo.png" />   
-                    </aside>     
+                    </aside> 
                     <main>
-                        <header>      
-                            <h2>Question <?php echo $index + 1; ?></h2>                            
-                        </header>
+                        <header> 
+                            <h2>Time for a Quizr</h2>
+                        </header> 
                         <section>
-                        
-                            <h3><?php echo $q->post_title; ?></h3>            
-
-                            <?php echo apply_filters( 'the_content', $q->post_content ); ?>
-
-                            <?php $answers = $quizr_answers_table->index( $q->ID );  ?>
-
-                            <div class="quizr-shortcode-question-set__answer-container">
-                                <?php foreach( $answers as $index => $value ) { ?>
-                                    <div>
-                                        <input 
-                                            type="checkbox" 
-                                            name="quizr_question_answer[<?php echo $index; ?>][is_correct]"
-                                            value="" 
-                                        />
-                                        <label><?php echo $value->description; ?></label>
-                                    </div>                      
-                                <?php } ?>
-                            </div>                            
-                        </section>
-                        <footer>
-                            
-                        </footer>
+                            <p>You will be shown some multiple choice questions</p>
+                            <p>Choose your answer and then move on.</p>    
+                            <p>Add some text here</p>
+                            <a class="quizr-shortcode-question-set__intro__start-quiz" href="">START QUIZ</a>
+                        </section>    
                     </main>
                 </article>
-            <?php } ?>
-            <div class="quizr-shortcode-question-set__arrows">
-                <a class="quizr-shortcode-question-set__arrows__prev hide">&#10094;</a>
-                <a class="quizr-shortcode-question-set__arrows__next hide show">&#10095;</a>
             </div>
-            <ul class="quizr-shortcode-question-set__pips">
-                <li class="quizr-shortcode-question-set__pips__pip active">&#8226;</li>
+            <div class="quizr-shortcode-question-set__questions hide">
                 <?php foreach( $questions as $index => $q ){ ?>
-                    <li class="quizr-shortcode-question-set__pips__pip"><a href="#">&#8226;</a></li>
+                    <article class="hide show">
+                        <aside>
+                            <img src="https://www.quizzer.dev.cc/wp-content/plugins/quizr/public/img/quizr-logo.png" />   
+                        </aside>     
+                        <main>
+                            <header>      
+                                <h2>Question <?php echo $index + 1; ?></h2>                            
+                            </header>
+                            <section>
+                            
+                                <h3><?php echo $q->post_title; ?></h3>            
+
+                                <?php echo apply_filters( 'the_content', $q->post_content ); ?>
+
+                                <?php $answers = $quizr_answers_table->index( $q->ID );  ?>
+
+                                <div class="quizr-shortcode-question-set__answer-container">
+                                    <?php foreach( $answers as $index => $value ) { ?>
+                                        <div>
+                                            <input 
+                                                type="checkbox" 
+                                                name="quizr_question_answer[<?php echo $index; ?>][is_correct]"
+                                                value="" 
+                                            />
+                                            <label><?php echo $value->description; ?></label>
+                                        </div>                      
+                                    <?php } ?>
+                                </div>                            
+                            </section>
+                            <footer>
+                                
+                            </footer>
+                        </main>
+                    </article>
                 <?php } ?>
-            </ul>
+                <div class="quizr-shortcode-question-set__arrows">
+                    <a class="quizr-shortcode-question-set__arrows__prev hide">&#10094;</a>
+                    <a class="quizr-shortcode-question-set__arrows__next hide show">&#10095;</a>
+                </div>
+                <ul class="quizr-shortcode-question-set__pips">
+                    <?php foreach( $questions as $index => $q ){ ?>
+                        <li class="quizr-shortcode-question-set__pips__pip"><a href="#">&#8226;</a></li>
+                    <?php } ?>
+                </ul>
+            </div>
         </div>
 
         <?php
