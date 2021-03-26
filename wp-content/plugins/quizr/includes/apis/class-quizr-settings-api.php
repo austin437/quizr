098194f78fb6d52e073cf47e1218829d6e89b11d
Robert Austin <austin437@hotmail.com>
2021-03-26 17:16:29 +0000

    ...

--- wp-content/plugins/quizr/includes/apis/class-quizr-settings-api.php
+++ wp-content/plugins/quizr/includes/apis/class-quizr-settings-api.php
@@ -4,18 +4,18 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class Quizr_Settings_Api {
 
+    public function register_settings() {
+        register_setting( 'quizr_options_group', 'quizr_max_answers_per_question', array( 'type' => 'string', 'sanitize_callback' => 'sanitize_text_field'  ) );
+        register_setting( 'quizr_options_group', 'quizr_show_cpt_question_in_menu', array( 'type' => 'string', 'sanitize_callback' => 'sanitize_text_field'  ) );        
+    }
+
     public function register_options_page() {
         add_options_page('Quizr Settings', 'Quizr', 'manage_options', 'quizr-settings', array( $this, 'render_options_page' ) );
     }
 
     public function render_options_page() {       
         require_once QUIZR_ADMIN_PATH . '/partials/quizr-admin-settings-page.php';
-    }
-
-    public function register_settings() {
-        register_setting( 'quizr_options_group', 'quizr_max_answers_per_question', array( 'type' => 'string', 'sanitize_callback' => 'sanitize_text_field'  ) );
-        register_setting( 'quizr_options_group', 'quizr_show_cpt_question_in_menu', array( 'type' => 'string', 'sanitize_callback' => 'sanitize_text_field'  ) );        
-    }
+    }    
 
     public function add_plugin_page_settings_link( $links ){
         $links[] = '<a href="' .
